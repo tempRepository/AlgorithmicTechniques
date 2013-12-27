450ad9d435bb51f0bcec1a8b11564a581ad8ebaa
Filip Jerzyna <filipjerzyna@gmail.com>
2013-12-27 12:24:44 +0100

    Implemented QuickExp and miller rabin prime test.

---
+++ AT_RSA/src/MillerRabin.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+import java.util.Random;
+
+public class MillerRabin {
+
+    public static Boolean isPrime(int number, int precision) {
+        int maxPowerOf2 = -1;
+        int powerIterator = 1;
+        while (true) {
+            if ((number - 1) % Math.pow(2, powerIterator) != 0) {
+                break;
+            } else {
+                maxPowerOf2 = powerIterator;
+                powerIterator++;
+            }
+        }
+        double d = (double) number / Math.pow(2, maxPowerOf2);
+        Random rand = new Random();
+        for (int k = 0; k < precision; k++) {
+            int randomNumber = 1;
+            while (randomNumber <= 1 || randomNumber >= number) {
+                randomNumber = rand.nextInt(number);
+            }
+            if (Math.pow(randomNumber, d) % number != 1) {
+                for (int r = 0; r <= maxPowerOf2 - 1; r++) {
+                    if (Math.pow(randomNumber, d * Math.pow(2, r)) % number != number - 1) {
+                        return true;
+                    }
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MillerRabin.isPrime(97, 10));
+    }
+
+}
--- AT_RSA/src/QuickExponentiation.java
+++ AT_RSA/src/QuickExponentiation.java
@@ -1,7 +1,25 @@
 
 public class QuickExponentiation {
-   public static int pow(int a, int b, int m)
+   public static double pow(int a, Integer b, int n)
    {
-       return 0;
+       String bits=Integer.toBinaryString(b);
+       int m=bits.length();
+       a= a%n;
+       double result=1;
+       int x=a;
+       for (int i = m-1; i >-1; i--) {
+        if (bits.charAt(i)=='1') {
+            result=result*x;
+            result=result % n;
+        }
+        x=x*x;
+        x=x % n;
+    }
+       return result;
    }
+   
+   public static void main(String[] args) {
+    System.out.println(Math.pow(20, 10)%7);
+    System.out.println(QuickExponentiation.pow(20, 10, 7));
+}
 }
